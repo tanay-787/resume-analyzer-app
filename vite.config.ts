b67ccc4ee314b663ded602bfe81d81cde4129245
Tanay <nevertrustesports@gmail.com>
2025-03-25 02:09:54 +0530

    fixed lock-file issues

--- vite.config.ts
+++ vite.config.ts
@@ -8,12 +8,12 @@ export default defineConfig({
     port: 3000, // Specify your desired port here
     host: true, // This makes the server listen on all addresses, including LAN and public addresses
   },
-  preview: {
-    port: 4173,
-    host: true,
-    // Allow all hosts to access the preview server
-    cors: true,
-    // Either use a wildcard or list all possible domains
-    allowedHosts: ['resume-analyzer-n10t.onrender.com', 'localhost', '.onrender.com', '*']
-  },
+  // preview: {
+  //   port: 4173,
+  //   host: true,
+  //   // Allow all hosts to access the preview server
+  //   cors: true,
+  //   // Either use a wildcard or list all possible domains
+  //   allowedHosts: ['resume-analyzer-n10t.onrender.com', 'localhost', '.onrender.com', '*']
+  // },
 })
